0c5d368713ca55f2c7689c856e087a74bb9ffdeb
ALEJANDRO <ALEJANDRO@192.168.1.136>
2014-09-06 10:45:01 +0100

    Funcionamiento estable. Clase y xml de OMC creada.

--- AppOrdenanzas/gen/aleso1982/app/appordenanzas/R.java
+++ AppOrdenanzas/gen/aleso1982/app/appordenanzas/R.java
@@ -26,14 +26,15 @@ public final class R {
         public static final int omc=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090007;
-        public static final int et1=0x7f090002;
-        public static final int lv1=0x7f090001;
-        public static final int spinner1=0x7f090000;
-        public static final int tvArticulo=0x7f090003;
-        public static final int tvCuantia=0x7f090006;
-        public static final int tvHecho=0x7f090004;
-        public static final int tvPuntos=0x7f090005;
+        public static final int action_settings=0x7f090008;
+        public static final int button1=0x7f090000;
+        public static final int et1=0x7f090007;
+        public static final int lv1=0x7f090006;
+        public static final int spinner1=0x7f090005;
+        public static final int tvArticulo=0x7f090001;
+        public static final int tvCuantia=0x7f090004;
+        public static final int tvHecho=0x7f090002;
+        public static final int tvPuntos=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- AppOrdenanzas/src/aleso1982/app/appordenanzas/MainActivity.java
+++ AppOrdenanzas/src/aleso1982/app/appordenanzas/MainActivity.java
@@ -1,129 +1,27 @@
 package aleso1982.app.appordenanzas;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Spinner;
+import android.view.View;
 
-public class MainActivity extends Activity implements TextWatcher {
+public class MainActivity extends Activity {
 
-	private Spinner spinner1;
-	private EditText et1;
-	private ListView lv1;
-
-	ArrayList<Fila> filtro = new ArrayList<Fila>();
-	ArrayList<Fila> fila = new ArrayList<Fila>();
-
-	MiAdaptador adapter;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		et1 = (EditText) findViewById(R.id.et1);
-		et1.addTextChangedListener(this);
-
-		spinner1 = (Spinner) findViewById(R.id.spinner1);
-		lv1 = (ListView) findViewById(R.id.lv1);
-		
-		getSpinnerOptions();
-
-		ArrayList<Fila> row = leerArticulos(obtenerJson());
-		filtro.addAll(row);
-		adapter = new MiAdaptador(this, filtro);
-		lv1.setAdapter(adapter);
-	}
-
-	private void getSpinnerOptions() {
-		String[] opciones = { "Articulo", "Hecho", "Cuantía", "Puntos" };
-		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_spinner_item, opciones);
-		spinner1.setAdapter(arrayAdapter);
 	}
 	
-	public JSONObject obtenerJson() {
-		JSONObject json = null; // Almacena el objeto JSON
-		StringBuilder stringBuilder; // Almacena el contenido del fichero JSON
-		String linea; // Almacena la línea leida del fichero JSON
-
-		try {
-			// Inicialización de campos
-			stringBuilder = new StringBuilder();
-			linea = null;
-			
-			// Obtiene el fichero JSON
-			InputStream fichero = 
-					getResources().openRawResource(R.raw.omc);
-
-			// Prepara el fichero para su lectura
-			BufferedReader bfreader = new BufferedReader(new InputStreamReader(
-					fichero));
-
-			// Lee el fichero JSON
-			while ((linea = bfreader.readLine()) != null) {
-				// Añade el contenido líena a línea
-				stringBuilder.append(linea + "\n");
-			}
-
-			// Cierra el fichero
-			fichero.close();
-			// Crea el objeto JSON
-			json = new JSONObject(stringBuilder.toString());
-		} catch (Exception ex) {
-		}
-		return json;
+	public void omc(View v){
+		Intent i = new Intent(this, Omc.class);
+		startActivity(i);
 	}
-
-	public ArrayList<Fila> leerArticulos(JSONObject json) {
-		String articulo, hecho, cuantia, puntos;
-
-		JSONArray arrayArticulos; // Almacena los articulos
-		JSONObject objeto; // Almacena un objeto
-
-		try {
-			objeto = new JSONObject();
-			// Almacena las filas
-			arrayArticulos = new JSONArray(json.getJSONArray("omc").toString());
-
-			// Recorre los articulos
-			for (int i = 0; i < arrayArticulos.length(); i++) {
-				// Obtine la fila del articulo
-				objeto = arrayArticulos.getJSONObject(i);
-				//objetoFila = objeto.getJSONObject("art");
-
-				// Almacena los datos del articulo
-				articulo = objeto.getString("Articulo");
-				hecho = objeto.getString("Hecho");
-				cuantia = objeto.getString("Cuantia");
-				puntos = objeto.getString("Puntos");
-
-				// Almacena el articulo obtenido
-				fila.add(new Fila("Art: " + articulo, hecho, "Cuantía: " + cuantia + "€", "Puntos: " + puntos));
-			}
-
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return fila;
-	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
@@ -139,61 +37,5 @@ public class MainActivity extends Activity implements TextWatcher {
 		return super.onOptionsItemSelected(item);
 	}
 
-	@Override
-	public void beforeTextChanged(CharSequence s, int start, int count,
-			int after) {
-
-	}
-
-	@Override
-	public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-	}
-
-	@Override
-	public void afterTextChanged(Editable s) {
-		String spinnerSelect = spinner1.getSelectedItem().toString();
-		String buscar = s.toString().toLowerCase();
-		filtro.clear();
-
-		if (spinnerSelect.equals("Articulo")) {
-			for (int i = 0; i < fila.size(); i++) {
-				if (fila.get(i).getArticulo().contains(buscar)) {
-					getFilterRow(i);
-				}
-			}
-		}
-
-		if (spinnerSelect.equals("Hecho")) {
-			for (int i = 0; i < fila.size(); i++) {
-				if (fila.get(i).getHecho().toLowerCase().contains(buscar)) {
-					getFilterRow(i);
-				}
-			}
-		}
-
-		if (spinnerSelect.equals("Cuantía")) {
-			for (int i = 0; i < fila.size(); i++) {
-				if (fila.get(i).getCuantia().contains(buscar)) {
-					getFilterRow(i);
-				}
-			}
-		}
-
-		if (spinnerSelect.equals("Puntos")) {
-			for (int i = 0; i < fila.size(); i++) {
-				if (fila.get(i).getPuntos().contains(buscar)) {
-					getFilterRow(i);
-				}
-			}
-		}
-
-		adapter.notifyDataSetChanged();
-	}
-
-	private void getFilterRow(int i) {
-		filtro.add(new Fila(fila.get(i).getArticulo(), fila.get(i).getHecho(),
-				fila.get(i).getCuantia(), fila.get(i).getPuntos()));
-	}
-
+	
 }
